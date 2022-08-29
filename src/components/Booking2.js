21c24dfafb2177182792e008594c6a14b0019eda
圈圈 <vvvvvvii@yahoo.com.tw>
2022-08-29 21:09:30 +0800

    課程結束

--- src/components/Booking2.js
+++ src/components/Booking2.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const Booking = () => {
+const Booking2 = () => {
   const [num, setNum] = useState(0)
   const handleNum = (type) =>{
     if(type=== 'minus'){
@@ -11,11 +11,11 @@ const Booking = () => {
   }
   return (
     <div>
-      <p>創始店訂位人數</p>
+      <p>加盟店訂位人數</p>
       <button onClick={()=>handleNum('minus')}>-</button>
         {num}
       <button onClick={()=>handleNum('plus')}>+</button>
     </div>
   );
 };
-export default Booking;
+export default Booking2;
--- src/components/Memo.js
+++ src/components/Memo.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Booking from "./Booking";
+import Booking2 from "./Booking2";
 
 function Memo() {
   return (
@@ -8,13 +9,14 @@ function Memo() {
       <h2>資料初始化與改變資料</h2>
       <h3>實際撰寫</h3>
       <Booking/>
-      {/* <h3>注意</h3>
+      <Booking2/>
+      <h3>注意</h3>
       <ul>
         <li>可以用 useState 裡帶的參數來初始化</li>
         <li>只能用 set 開頭的函式命名 setState ，他被呼叫的時候會負責更改 State 值</li>
         <li>State 被改變時，會觸發重新渲染</li>
         <li>State 是單獨存在於元件裡的，在其他元件抓不到</li>
-      </ul> */}
+      </ul>
     </div>
   )
 }
