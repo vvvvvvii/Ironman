b9b133a38756292308828368e93a7bd7ce8ddf78
圈圈 <vvvvvvii@yahoo.com.tw>
2022-09-03 17:05:38 +0800

    課前準備

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -1,44 +1,18 @@
 import React from 'react';
 import { useState } from 'react';
-import Booking from "./Booking";
+import Menu from './Menu';
 
 function Memo() {
-  const [parentData, setParentData] = useState("父元件創立的資料");
-  const change = (parameter) =>{
-    setParentData(parameter)
-  }
   return (
     <div>
-      <h1>Props</h1>
-      <h2>「單向傳遞」父元件到子元件的資料</h2>
+      <h1>重複渲染、條件渲染、預設值、 dangerouslySetInnerHTML</h1>
       <h3>實際撰寫</h3>
-      <p>父元件： {parentData}</p>
-      <Booking data={parentData} setParentData={setParentData} change={change}/>
-      <h3>注意</h3>
+      <Menu/>
       <ul>
-        <li>單向傳遞原則</li>
-        <li>和 State 的差別</li>
-         <table>
-          <thead>
-            <tr>
-              <td></td>
-              <td>State</td>
-              <td>Props</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>控制</td>
-              <td>自己本身</td>
-              <td>讓父元件來控制自己</td>
-            </tr>
-            <tr>
-              <td>改變</td>
-              <td>可被改變</td>
-              <td>唯讀、不可改變</td>
-            </tr>
-          </tbody>
-        </table> 
+        <li>重複渲染： map</li>
+        <li>條件渲染： &&</li>
+        <li>預設值： ||</li>
+        <li>dangerouslySetInnerHTML</li>
       </ul>
     </div>
   )
---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <div className='box'>
+      <h3>菜單</h3>
+    </div>
+  )
+}
+
+export default Menu
