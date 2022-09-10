32846bc1f327f46aa625e5f45375656963be902e
圈圈 <vvvvvvii@yahoo.com.tw>
2022-09-10 11:55:19 +0800

    課程結束

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,22 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
 const Booking = () => {
-  const [num, setNum] = useState(0)
-  const handleNum = (type) =>{
-    if(type=== 'minus'){
-      setNum(num-1)
-    }else{
-      setNum(num+1)
+  const [num, setNum] = useState(0);
+  const handleNum = (type) => {
+    if (type === "minus") {
+      setNum(num - 1);
+      // console.log("set:", num);
+    } else {
+      setNum(num + 1);
     }
-  }
+  };
+  useEffect(() => {
+    console.log("useEffect:", num);
+  }, [num]);
+  useEffect(() => {
+    console.log("first render");
+    return () => {
+      console.log("unmount");
+    };
+  }, []);
   return (
     <div>
       <p>創始店訂位人數</p>
-      <button onClick={()=>handleNum('minus')}>-</button>
-        {num}
-      <button onClick={()=>handleNum('plus')}>+</button>
+      <button onClick={() => handleNum("minus")}>-</button>
+      {num}
+      <button onClick={() => handleNum("plus")}>+</button>
     </div>
   );
 };
 export default Booking;
-
