4779b4567c2bab68f21ff9d6a953f2259e2477b1
圈圈 <vvvvvvii@yahoo.com.tw>
2022-09-10 19:30:21 +0800

    課前準備

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -3,9 +3,8 @@ import React from "react";
 function Memo() {
   return (
     <div>
-      <h1>建立 JSON Server 用 useEffect 模擬抓取、送出與刪除資料</h1>
+      <h1>客製化 Hook</h1>
       <h3>實際撰寫</h3>
-      <p>npx json-server --watch data/db.json --port 8000</p>
     </div>
   );
 }
