2c23e9c71946775f7e296c012d058fb6de4cfa58
圈圈 <vvvvvvii@yahoo.com.tw>
2022-09-11 20:08:41 +0800

    課程結束

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import Home from "./pages/Home";
 import Memo from "./pages/Memo";
 import MenuList from "./pages/MenuList";
 import Navbar from "./components/Navbar";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
       {/* <Memo /> */}
-      <Navbar />
-      <Home />
-      <MenuList />
+      <HashRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/menu-list">
+            <MenuList />
+          </Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <ul className="navbar">
       <li className="nav-item">
-        <a href="/">首頁</a>
+        <Link to="/">首頁</Link>
       </li>
       <li className="nav-item">
-        <a href="/menu-list">菜單</a>
+        <Link to="/menu-list">菜單</Link>
       </li>
     </ul>
   );
