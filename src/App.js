f54054004dba608406baf68d1749fdb885348b36
圈圈 <vvvvvvii@yahoo.com.tw>
2022-09-24 16:57:23 +0800

    課前準備

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-// import Home from "./pages/Home";
 import Memo from "./pages/Memo";
-// import MenuList from "./pages/MenuList";
-// import MenuItem from "./pages/MenuItem";
-// import NotFound from "./pages/NotFound";
-// import Navbar from "./components/Navbar";
-// import { HashRouter, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="app-container">
       <Memo />
-      {/* <HashRouter>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/menu-list">
-            <MenuList />
-          </Route>
-          <Route path="/menu-item/:id">
-            <MenuItem />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </HashRouter> */}
     </div>
   );
 }
--- src/pages/Memo.js
+++ src/pages/Memo.js
@@ -1,40 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
+// import ComponentA from "../components/componentA";
+// import ComponentB from "../components/componentB";
 
 function Memo() {
   // const condition = true;
   const condition = false;
-  const [show, setShow] = useState(false);
-  const styles = { fontSize: "20px", color: "#333" };
+
   return (
     <div>
-      <h1>引入 CSS ： inline-style</h1>
+      <h1>引入 CSS ： 外部引入</h1>
       <h2>使用與限制</h2>
-      <div>
-        <h3>實際撰寫</h3>
-        <ul>
-          <li style={{ color: "#333" }}>外層包大括號＋大括號</li>
-          <li style={{ fontSize: "20px" }}>屬性有 dash 的改小駝峰寫法</li>
-          <li style={{ lineHeight: 1.8 }}>以數字或字串傳遞值</li>
-          <li>搭配三元運算子判斷條件，再套用樣式</li>
-          <p style={{ color: condition ? "#333" : "#600" }}>example 1</p>
-          <p style={{ fontSize: "20px", color: condition ? "#333" : "#600" }}>
-            example 2
-          </p>
-          <p style={condition ? { fontSize: "20px", color: "#333" } : {}}>
-            example 3
-          </p>
-          <li style={styles}>樣式物件也能抽出單獨撰寫</li>
-        </ul>
-        <h3>注意</h3>
-        <ul>
-          <li>能不渲染就不渲染</li>
-          {/* 比較差的做法：網頁需要做好幾件事（渲染、判斷、加樣式）才能達成目的 */}
-          <p style={{ display: show ? "inline" : "none" }}>Hello</p>
-          {/* 比較好的做法：不符合條件的話一開始就不渲染 */}
-          {show && <p>Hi</p>}
-          <li>無法實現 hover 、 media-query</li>
-        </ul>
-      </div>
+      <h3>實際撰寫</h3>
+      <ul>
+        <li>用習慣的方式撰寫 CSS</li>
+        <li>屬性不用改小駝峰寫法</li>
+        <li>可以實現 hover 、 media-query</li>
+        <li>在要用的元件 import ，並搭配 className 套用</li>
+        <li>搭配三元運算子判斷條件，再套用樣式</li>
+        <p>example 1</p>
+        <p>example 2</p>
+      </ul>
+      <h3>注意</h3>
+      <ul>
+        <li>CSS 不會只對應到引入的元件</li>
+        {/* <ComponentB />
+        <ComponentA /> */}
+        <li>CSS 載入的順序依照被 import 的順序，不是元件使用的順序</li>
+      </ul>
     </div>
   );
 }
