db2ef3cb8afdf917f25d129576633449bf789452
圈圈 <vvvvvvii@yahoo.com.tw>
2022-09-24 18:06:22 +0800

    課前準備

--- src/pages/Memo.js
+++ src/pages/Memo.js
@@ -1,26 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-import ComponentA from "../components/componentA";
-import ComponentB from "../components/componentB";
 
 function Memo() {
   return (
     <div>
-      <h1>引入 CSS ： Styled Components</h1>
-      <h3>安裝</h3>
-      <p>npm install --save styled-components</p>
-      <p>Emmet: vscode-styled-components 套件</p>
-      <h3>實際撰寫</h3>
+      <h1>Styled Components 與 props 、 extend 、共用元件</h1>
+      <h3>props ：設定會不斷用到的主題色等</h3>
+      <Button>example 1</Button>
+      <Button>example 2</Button>
+      <Button>example 3</Button>
+      <h3>extend ：套用他人已寫好的樣式，再客製化</h3>
       <Button>example</Button>
-      <h3>好處</h3>
-      <ul>
-        <li>不會有覆蓋問題</li>
-        <ComponentA />
-        <ComponentB />
-        <li>只載入相關樣式</li>
-        <li>和 React 元件化的概念相符</li>
-      </ul>
-      <h3>注意：樣式亂數問題與解法</h3>
     </div>
   );
 }
