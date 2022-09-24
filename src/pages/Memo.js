099ab9701a472c7a7bd0c55a1250a6a9c39d576e
圈圈 <vvvvvvii@yahoo.com.tw>
2022-09-24 16:53:47 +0800

    課程結束

--- src/pages/Memo.js
+++ src/pages/Memo.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
 function Memo() {
-  const condition = true;
-  // const condition = false;
-  const [show, setShow] = useState(true);
-
+  // const condition = true;
+  const condition = false;
+  const [show, setShow] = useState(false);
+  const styles = { fontSize: "20px", color: "#333" };
   return (
     <div>
       <h1>引入 CSS ： inline-style</h1>
@@ -12,14 +12,18 @@ function Memo() {
       <div>
         <h3>實際撰寫</h3>
         <ul>
-          <li>外層包大括號＋大括號</li>
-          <li>屬性有 dash 的改小駝峰寫法</li>
-          <li>以數字或字串傳遞值</li>
+          <li style={{ color: "#333" }}>外層包大括號＋大括號</li>
+          <li style={{ fontSize: "20px" }}>屬性有 dash 的改小駝峰寫法</li>
+          <li style={{ lineHeight: 1.8 }}>以數字或字串傳遞值</li>
           <li>搭配三元運算子判斷條件，再套用樣式</li>
-          <p>example 1</p>
-          <p>example 2</p>
-          <p>example 3</p>
-          <li>樣式物件也能抽出單獨撰寫</li>
+          <p style={{ color: condition ? "#333" : "#600" }}>example 1</p>
+          <p style={{ fontSize: "20px", color: condition ? "#333" : "#600" }}>
+            example 2
+          </p>
+          <p style={condition ? { fontSize: "20px", color: "#333" } : {}}>
+            example 3
+          </p>
+          <li style={styles}>樣式物件也能抽出單獨撰寫</li>
         </ul>
         <h3>注意</h3>
         <ul>
@@ -27,6 +31,7 @@ function Memo() {
           {/* 比較差的做法：網頁需要做好幾件事（渲染、判斷、加樣式）才能達成目的 */}
           <p style={{ display: show ? "inline" : "none" }}>Hello</p>
           {/* 比較好的做法：不符合條件的話一開始就不渲染 */}
+          {show && <p>Hi</p>}
           <li>無法實現 hover 、 media-query</li>
         </ul>
       </div>
