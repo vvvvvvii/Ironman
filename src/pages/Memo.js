e04bbab7f90f0db9b836fbf2f8e5fb3af7423ec5
圈圈 <vvvvvvii@yahoo.com.tw>
2022-10-01 12:44:59 +0800

    課前準備

--- src/pages/Memo.js
+++ src/pages/Memo.js
@@ -6,45 +6,27 @@ function Memo() {
   return (
     <div>
       <h1>在 React 中如何使用 Bootstrap</h1>
-      <h3>介紹 Bootstrap</h3>
-      <h3>安裝</h3>
-      <a href="https://react-bootstrap.github.io/getting-started/introduction/">
-        React Bootstrap 官網
-      </a>
-      <p>npm install react-bootstrap bootstrap</p>
       <h3>實際撰寫</h3>
-      {/* 引入 */}
-      <Button>test1</Button>
-      {/* 客製化 variant & extend */}
-      <Button variant="secondary">test2</Button>
-      <CustomButton>test2</CustomButton>
-      {/* global styles */}
-      <a href="https://react-bootstrap.github.io/getting-started/introduction/">
-        {" "}
-        test link
-      </a>
-      {/* hover */}
-      <CustomButton>test2</CustomButton>
-      {/* props & theme */}
-      <div>
-        <CustomButton2 theme={theme}>test2</CustomButton2>
-      </div>
+      {/* utils */}
+      <Button className="mt-1">test1</Button>
+      <p className="fz-1">example</p>
+      <h3>theme 放在不同地方的差別</h3>
     </div>
   );
 }
-const theme = {
-  primary: "black",
-  secondary: "red",
-};
+// const theme = {
+//   primary: "black",
+//   secondary: "red",
+// };
 
-const CustomButton = styled(Button)`
-  font-weight: bold;
-  font-size: 32px;
-  &:hover {
-    background: #000 !important;
-  }
-`;
-const CustomButton2 = styled(Button)`
-  background: ${(props) => props.theme.secondary};
-`;
+// const CustomButton = styled(Button)`
+//   font-weight: bold;
+//   font-size: 32px;
+//   &:hover {
+//     background: #000 !important;
+//   }
+// `;
+// const CustomButton2 = styled(Button)`
+//   background: ${(props) => props.theme.secondary};
+// `;
 export default Memo;
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,6 +1,30 @@
 import { createGlobalStyle } from "styled-components";
+import styled, { css } from "styled-components";
+
+const fontSizeBase = "1";
+const defaultSpacer = "1";
+
+const theme = {
+  spacers: [
+    0,
+    defaultSpacer * 0.25,
+    defaultSpacer * 0.75,
+    defaultSpacer * 1,
+    defaultSpacer * 1.2,
+    defaultSpacer * 1.5,
+  ],
+  fonts: [
+    0,
+    fontSizeBase * 0.25,
+    fontSizeBase * 0.75,
+    fontSizeBase * 1,
+    fontSizeBase * 1.2,
+    fontSizeBase * 1.5,
+  ],
+};
+
 const GlobalStyles = createGlobalStyle`
-  a{
+a{
     text-decoration: none;
   }
 `;
