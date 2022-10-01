b6a64e8da182d782b4a4f184b11333560b09e0d2
圈圈 <vvvvvvii@yahoo.com.tw>
2022-10-01 18:21:39 +0800

    課前準備

--- src/pages/Memo.js
+++ src/pages/Memo.js
@@ -1,49 +1,24 @@
 import React from "react";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import AbcIcon from "@mui/icons-material/Abc";
 
 function Memo() {
   return (
     <div>
       <h1>Material UI</h1>
-      <h2>元件介紹、 Material Icon</h2>
-      <h3>安裝</h3>
-      <p>官網：https://mui.com/material-ui/getting-started/overview/</p>
-      <p>npm install @mui/material @emotion/react @emotion/styled</p>
-      <h3>Box</h3>
-      <Box
-        mt={2}
-        sx={{
-          width: 300,
-          height: 300,
-          backgroundColor: "primary.dark",
-          "&:hover": {
-            backgroundColor: "primary.main",
-            opacity: [0.9, 0.8, 0.7],
-          },
-        }}
-      >
-        123
-      </Box>
-      <h3>Typography</h3>
-      <Typography variant="h1">h1. Heading</Typography>
+      <h2>客製化樣式</h2>
+      <h3>Theme & Typography</h3>
+      <Typography variant="h1" component="h2">
+        custom theme
+      </Typography>
       <Typography variant="h1" component="h2">
         h1. Heading
       </Typography>
-      <h3>Link</h3>
-      <h3>Button</h3>
-      <h3>Material Icon</h3>
-      <p>安裝：npm install @mui/icons-material</p>
-      <Button variant="contained" color="success">
+      <h3>Theme & Spacing</h3>
+      <h3>Platte 顏色</h3>
+      <Button variant="contained" color="secondary">
         123
       </Button>
-      <IconButton color="primary" aria-label="upload picture" component="label">
-        <input hidden accept="image/*" type="file" />
-        <AbcIcon />
-      </IconButton>
     </div>
   );
 }
