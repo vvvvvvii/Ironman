a12e0e8e4c5f28bbb5ccf2928d8448983b368f48
圈圈 <vvvvvvii@yahoo.com.tw>
2022-10-01 14:38:31 +0800

    課前準備

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import GlobalStyles from "./styles/globalStyles";
-import "./App.css";
+// import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyles></GlobalStyles>
     <App />
   </React.StrictMode>
 );
--- src/pages/Memo.js
+++ src/pages/Memo.js
@@ -1,33 +1,21 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import styled, { css } from "styled-components";
 
 function Memo() {
   return (
     <div>
-      <h1>在 React 中如何使用 Bootstrap</h1>
-      <h3>實際撰寫</h3>
-      {/* utils */}
-      <Button className="mt-1">test1</Button>
-      <p className="fz-1">example</p>
-      <h3>theme 放在不同地方的差別</h3>
-      <CustomButton2 theme={theme}>test1</CustomButton2>
+      <h1>Material UI</h1>
+      <h2>元件介紹、 Material Icon</h2>
+      <h3>安裝</h3>
+      <p>官網：https://mui.com/material-ui/getting-started/overview/</p>
+      <p>npm install @mui/material @emotion/react @emotion/styled</p>
+      <h3>Box</h3>
+      <h3>Typography</h3>
+      <h3>Link</h3>
+      <h3>Button</h3>
+      <h3>Material Icon</h3>
+      <p>安裝：npm install @mui/icons-material</p>
     </div>
   );
 }
-const theme = {
-  primary: "black",
-  secondary: "red",
-};
 
-// const CustomButton = styled(Button)`
-//   font-weight: bold;
-//   font-size: 32px;
-//   &:hover {
-//     background: #000 !important;
-//   }
-// `;
-const CustomButton2 = styled(Button)`
-  background: ${(props) => props.theme.secondary};
-`;
 export default Memo;
