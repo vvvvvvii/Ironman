186b8bf2634c32bc5ee004593f1cd7e9f70ec304
圈圈 <vvvvvvii@yahoo.com.tw>
2022-10-01 18:01:31 +0800

    課程結束

--- src/pages/Memo.js
+++ src/pages/Memo.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import AbcIcon from "@mui/icons-material/Abc";
 
 function Memo() {
   return (
@@ -9,11 +14,36 @@ function Memo() {
       <p>官網：https://mui.com/material-ui/getting-started/overview/</p>
       <p>npm install @mui/material @emotion/react @emotion/styled</p>
       <h3>Box</h3>
+      <Box
+        mt={2}
+        sx={{
+          width: 300,
+          height: 300,
+          backgroundColor: "primary.dark",
+          "&:hover": {
+            backgroundColor: "primary.main",
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
+      >
+        123
+      </Box>
       <h3>Typography</h3>
+      <Typography variant="h1">h1. Heading</Typography>
+      <Typography variant="h1" component="h2">
+        h1. Heading
+      </Typography>
       <h3>Link</h3>
       <h3>Button</h3>
       <h3>Material Icon</h3>
       <p>安裝：npm install @mui/icons-material</p>
+      <Button variant="contained" color="success">
+        123
+      </Button>
+      <IconButton color="primary" aria-label="upload picture" component="label">
+        <input hidden accept="image/*" type="file" />
+        <AbcIcon />
+      </IconButton>
     </div>
   );
 }
