cc9d2ce4cb2e45657f9b5860fc6c6b9d76f0f4b1
圈圈 <vvvvvvii@yahoo.com.tw>
2022-10-01 18:45:21 +0800

    課程結束

--- src/pages/Memo.js
+++ src/pages/Memo.js
@@ -1,6 +1,25 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: 12,
+    },
+    h2: {
+      fontSize: 20,
+    },
+  },
+  spacing: [0, 4, 80, 16, 32, 64],
+  palette: {
+    secondary: {
+      main: "#ff4400",
+    },
+  },
+});
 
 function Memo() {
   return (
@@ -8,17 +27,25 @@ function Memo() {
       <h1>Material UI</h1>
       <h2>客製化樣式</h2>
       <h3>Theme & Typography</h3>
-      <Typography variant="h1" component="h2">
-        custom theme
-      </Typography>
+      <ThemeProvider theme={theme}>
+        <Typography variant="h1" component="h2">
+          custom theme
+        </Typography>
+      </ThemeProvider>
       <Typography variant="h1" component="h2">
         h1. Heading
       </Typography>
       <h3>Theme & Spacing</h3>
-      <h3>Platte 顏色</h3>
-      <Button variant="contained" color="secondary">
-        123
-      </Button>
+      <ThemeProvider theme={theme}>
+        <Typography variant="h1" component="h2" mt={2}>
+          custom theme
+        </Typography>
+
+        <h3>Platte 顏色</h3>
+        <Button variant="contained" color="secondary">
+          123
+        </Button>
+      </ThemeProvider>
     </div>
   );
 }
