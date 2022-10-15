b16d63e5a3edcf0598ace90631f4a38bd59785c0
圈圈 <vvvvvvii@yahoo.com.tw>
2022-10-15 12:08:34 +0800

    課前準備

--- src/pages/Memo.js
+++ src/pages/Memo.js
@@ -1,24 +1,16 @@
 import React from "react";
-import FocusInput from "../components/FocusInput";
-import HookTimer from "../components/HookTimer";
-import PrevData from "../components/PrevData";
 
 function Memo() {
   return (
     <div>
-      <h1>useRef</h1>
-      <h3>特性：</h3>
-      <ul>
-        <li>會回傳一個含有 current 的物件</li>
-        <li>可以操作 DOM</li>
-        <li>值被更新時，不會觸發元件重新渲染</li>
-      </ul>
-      <h3>操作 DOM</h3>
-      <FocusInput />
-      <h3>阻止無限迴圈</h3>
-      <HookTimer />
-      <h3>取得過去的值</h3>
-      <PrevData />
+      <h1>Redux ：介紹、安裝與使用</h1>
+      <h3>官網</h3>
+      <a href="https://react-redux.js.org/introduction/getting-started#an-existing-react-app">
+        連結
+      </a>
+      <h3>安裝</h3>
+      <p>npm install redux react-redux</p>
+      <h3>撰寫</h3>
     </div>
   );
 }
