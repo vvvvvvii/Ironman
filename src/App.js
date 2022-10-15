ed6980988395b1bec9fb49bdbfc84432df027355
圈圈 <vvvvvvii@yahoo.com.tw>
2022-10-15 17:04:56 +0800

    課前準備

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import Memo from "./pages/Memo";
-import Counter from "./components/Counter";
-import ComponentA from "./components/ComponentA";
-import React, { useReducer } from "react";
 
-export const CountContext = React.createContext();
-const initialState = 0;
-const reducer = (state, action) => {
-  switch (action) {
-    case "add":
-      return state + 1;
-    default:
-      return state;
-  }
-};
 export default function App() {
-  const [count, dispatch] = useReducer(reducer, initialState);
-
   return (
     <div className="app-container">
-      <CountContext.Provider
-        value={{ countState: count, countDispatch: dispatch }}
-      >
-        <Memo />
-        <Counter />
-        <ComponentA />
-      </CountContext.Provider>
+      <Memo />
     </div>
   );
 }
--- src/pages/Memo.js
+++ src/pages/Memo.js
@@ -1,28 +1,37 @@
 import React from "react";
-import Reducer from "../components/Reducer";
-import Reducer2 from "../components/Reducer2";
-import Reducer3 from "../components/Reducer3";
-
-import { useState } from "react";
 
 function Memo() {
-  // const [num, setNum] = useState(0);
   return (
     <div>
-      <h1>useReducer & useContext</h1>
-      <h2>用 useReducer 來管理 State / useContext 傳遞全域 State </h2>
-      <h3>useReducer</h3>
+      <h1>在 Github 上傳 React 、在 Code Sandbox 使用 React</h1>
+      <h3>上傳 Github 步驟</h3>
       <ul>
-        <li>提供初始值，與改變 State 的函式</li>
-        <li>能依據傳入不同的 action 值，用不同方式改變 State</li>
+        <li>
+          1. 連進你的 Github 創建一個儲存庫（如果你已經做好，可以跳過這步）
+        </li>
+        <li>2. 創建你的 React 專案（如果你已經做好，可以跳過這步）</li>
+        <li>
+          3. 安裝 gh-pages package ：<p>npm install gh-pages --save-dev</p>
+        </li>
+        <li>
+          4. 在 package.json 加上幾行：
+          <p>"homepage": "https://username.github.io/repo-name",</p>
+          <p>"predeploy": "npm run build",</p>
+          <p>"deploy": "gh-pages -d build",</p>
+        </li>
+        <li>
+          5. 使用 git remote 將本地端連上遠端（如果你已經連接過，可以跳過這步）
+          <p>git remote add origin https://username.github.io/repo-name .git</p>
+        </li>
+        <li>
+          6. 只需在終端機輸入此指令，就會同時跑 predeploy 和 deploy ：
+          <p>npm run deploy</p>
+          {/* 自己設定 commit message */}
+          {/* <p>npm run deploy -- -m "Deploy React app to GitHub Pages"</p> */}
+        </li>
+        <li>7. 在 Github 的設定把預設分支設成 gh-pages 並把儲存庫設為公開</li>
       </ul>
-      <h3>寫法</h3>
-      <Reducer />
-      <h3>傳參數與共用 reducer</h3>
-      <Reducer2 />
-      <h3>重複使用 reducer</h3>
-      <Reducer3 />
-      <h3>搭配 useContext 傳遞全域 State </h3>
+      <h3>Code Sandbox 介紹</h3>
     </div>
   );
 }
